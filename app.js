4a94a2b19d93f6e452795665129c08ea472059c0
Samantha Ready <sready@salesforce.com>
2014-11-04 15:47:45 -0800

    added client key

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ angular.module('demoApp', ["controllers", "forceng"])
             // optional: defaults to 'v30.0'
             apiVersion: 'v30.0',
             // optional: defaults to oauthcallback.html in same directory as index.html
-            // oauthRedirectURL: 'http://localhost:3000/forceng/oauthcallback.html',
+            oauthRedirectURL: 'http://samforceng.herokuapp.com/oauthcallback.html',
             // only required when hosting app on your own server to work around cross-domain issues (Not required when running locally on mobile device)
-            proxyURL: 'https://samforceng.herokuapp.com/' /* Node.js hosted proxy provided as a sample for development only https://github.com/ccoenraets/cors-proxy */
+            //proxyURL: 'https://samforceng.herokuapp.com/' /* Node.js hosted proxy provided as a sample for development only https://github.com/ccoenraets/cors-proxy */
             // proxyURL: 'https://sfdc-cors.herokuapp.com' /* Play/Scala hosted proxy provided as a sample for development only https://github.com/jamesward/sf-cors-proxy */
             // proxyURL: 'http://localhost:3000' /* Your own local proxy for development */
 
